0a6fbef2bff435df9e26211cb7dd0fd660ef3ce2
Okky Dwi Triferyawan <61794853+okky-chan@users.noreply.github.com>
2021-12-27 15:25:46 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include "a-star.cpp"
+using namespace std;
+
+#define ROW 9
+#define COL 10
+
+int main()
+{
+	/* Description of the Grid-
+	1--> The cell is not blocked
+	0--> The cell is blocked */
+	int grid[ROW][COL]
+		= { { 1, 0, 1, 1, 1, 1, 0, 1, 1, 1 },
+			{ 1, 1, 1, 0, 1, 1, 1, 0, 1, 1 },
+			{ 1, 1, 1, 0, 1, 1, 0, 1, 0, 1 },
+			{ 0, 0, 1, 0, 1, 0, 0, 0, 0, 1 },
+			{ 1, 1, 1, 0, 1, 1, 1, 0, 1, 0 },
+			{ 1, 0, 1, 1, 1, 1, 0, 1, 0, 0 },
+			{ 1, 0, 0, 0, 0, 1, 0, 0, 0, 1 },
+			{ 1, 0, 1, 1, 1, 1, 0, 1, 1, 1 },
+			{ 1, 1, 1, 0, 0, 0, 1, 0, 0, 1 } };
+
+	// Source is the left-most bottom-most corner
+	Pair src = make_pair(8, 0);
+
+	// Destination is the left-most top-most corner
+	Pair dest = make_pair(0, 0);
+
+	aStarSearch(grid, src, dest);
+
+	return (0);
+}
